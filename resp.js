c131fda7a8b22c171ad8157dc5aa9f808a327a96
Narayan Rajkumar Suryawanshi <178514243+NARAYAN49-tech@users.noreply.github.com>
2025-02-12 09:38:10 +0530

    Add files via upload
    This is my self build webPage

---
+++ resp.js
@@ -0,0 +1,49 @@
+// let burger = document.querySelectorAll('button');
+// let navbar = document.querySelector('.navbar');
+// let rightNav = document.querySelector('.burger');
+// let navList = document.querySelector('nav-list');
+
+// let search = document.getElementById("search");
+
+// var calendars = require('world-calendars');
+
+// var gregorian = calendars.instance();
+// var nepali = calendars.instance('nepali');
+
+// // gives nepali date 2073-07-15
+// var nepaliHalloween = nepali.fromJD(gregorian.newDate(2016, 10, 31).toJD());
+// search.ariaValueText = nepaliHalloween;
+// let subscribe = document.querySelector(".btn");
+// document.addEventListener("mouseenter",() => {
+//     subscribe.style.backgroundColor = "grey";
+// });
+// let btn = document.getElementsByTagName("button");
+// .addEventListener("mouseenter",()=>{
+    
+// })
+
+
+var typed = new Typed("#spanElement", {
+    strings: ["Students","Teachers","Labours","Workers","Developers","Coders"],
+    typeSpeed: 90,
+    startDelay: 500,
+    backDelay: 1000,
+    backSpeed: 15,
+    loop: true,
+    loopCount: 2,
+    showCursor: true,
+    cursorChar:"."
+});
+
+
+var typed = new Typed("title", {
+    strings: ["Students","Teachers","Labours","Workers","Developers","Coders"],
+    typeSpeed: 90,
+    startDelay: 500,
+    backDelay: 1000,
+    backSpeed: 15,
+    loop: true,
+    loopCount: 2,
+    showCursor: true,
+    cursorChar:"."
+});
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,37 @@
+// let button  = "         Narayan ";
+// let surma = button.trim();
+// console.log(surma);
+// let verma = surma.concat(" Suryawanshi");
+// console.log(verma);
+
+// // let sing = setInterval(() => {
+// //     console.log("is there anyone");
+// // }, 2000);
+// // let v = setTimeout((r) => {
+// //     console.log("Han boliye");
+// //     clearInterval(sing);
+// // }, 11000);
+// // console.log(v);
+// const student = {
+//     name: "aman",
+//     mark: 95,
+//     prop: this,
+//     getName : function(){
+//         console.log(this.name);
+//         return this.name;
+//     },
+//     getMarks : function(){
+//         console.log(this.mark);
+//     },
+//     getInfo1: function() {
+//         setTimeout(() => {
+//             return this.name;
+//         },2000);
+//     },
+//     getInfo2: function() {
+//         setTimeout(() => {
+//             return this.marks;
+//         },2000);
+//     }
+// };
+// console.log(student);
